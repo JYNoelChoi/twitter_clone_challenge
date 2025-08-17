d7f29216c1f40e2e2926a44d3c116fc89c7d35fe
JYNoelChoi <noelchoi@hotmail.com>
2025-08-17 22:07:35 +0800

    password screen

--- lib/features/authentication/widgets/password_screen.dart
+++ lib/features/authentication/widgets/password_screen.dart
@@ -1,10 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/gaps.dart';
+import 'package:twitter_clone/constants/sizes.dart';
+import 'package:twitter_clone/features/authentication/widgets/auth_button.dart';
+import 'package:twitter_clone/features/authentication/widgets/twitter_appbar.dart';
+import 'package:twitter_clone/features/customization/widgets/interest_screen.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+  String _password = "";
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  bool _isPasswordValid() {
+    return _password.isNotEmpty && _password.length > 8;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (!_isPasswordValid()) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const InterestScreen()),
+    );
+  }
+
+  // void _onClearTap() {
+  //   _passwordController.clear();
+  // }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: TwitterAppBar(showLeading: false),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              Text(
+                "You'll need a password",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Gaps.v16,
+              Text(
+                "Make sure it's 8 characters or more",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                controller: _passwordController,
+                autocorrect: false,
+                onEditingComplete: _onSubmit,
+                obscureText: _obscureText,
+                cursorColor: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // GestureDetector(
+                      //   onTap: _onClearTap,
+                      //   child: FaIcon(
+                      //     FontAwesomeIcons.solidCircleXmark,
+                      //     color: Colors.grey.shade500,
+                      //     size: Sizes.size20,
+                      //   ),
+                      // ),
+                      Gaps.h16,
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: FaIcon(
+                          _obscureText
+                              ? FontAwesomeIcons.eye
+                              : FontAwesomeIcons.eyeSlash,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                      Gaps.h8,
+                      Container(
+                        child: _isPasswordValid()
+                            ? FaIcon(
+                                FontAwesomeIcons.solidCircleCheck,
+                                color: Colors.green,
+                                size: Sizes.size20,
+                              )
+                            : null,
+                      ),
+                    ],
+                  ),
+                  hintText: "Password",
+                  // errorText: "xx",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: _onSubmit,
+                      child: AuthButton(
+                        text: "Next",
+                        buttonColor: !_isPasswordValid()
+                            ? Colors.black54
+                            : Colors.black,
+                        textColor: !_isPasswordValid()
+                            ? Colors.grey.shade400
+                            : Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/authentication/widgets/twitter_appbar.dart
+++ lib/features/authentication/widgets/twitter_appbar.dart
@@ -3,25 +3,28 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:twitter_clone/constants/sizes.dart';
 
 class TwitterAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TwitterAppBar({super.key});
+  final bool showLeading;
+  const TwitterAppBar({super.key, this.showLeading = false});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: GestureDetector(
-        onTap: () => {Navigator.of(context).pop()},
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size16,
-            horizontal: Sizes.size16,
-          ),
-          child: FaIcon(
-            FontAwesomeIcons.arrowLeft,
-            color: Colors.black87,
-            size: Sizes.size20,
-          ),
-        ),
-      ),
+      leading: showLeading
+          ? GestureDetector(
+              onTap: () => {Navigator.of(context).pop()},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size16,
+                  horizontal: Sizes.size16,
+                ),
+                child: FaIcon(
+                  FontAwesomeIcons.arrowLeft,
+                  color: Colors.black87,
+                  size: Sizes.size20,
+                ),
+              ),
+            )
+          : null,
       automaticallyImplyLeading: false,
 
       backgroundColor: Colors.white,
